83c8cc7f393fd1f772a504875f9e55f1bf5779b6
fabiobosisio <fbosisio@gmail.com>
2022-12-07 20:24:52 -0300

    Versão estável

--- ampatch.js
+++ ampatch.js
@@ -2,46 +2,101 @@
 
 const Automerge = require('automerge') // Carrega o automerge no node
 var fs = require("fs"); // file system
+let verbose = false; // Ativa (true) e desativa (false) o modo verboso
+let diff; // Variável para armazenar a diferença
+let ant; // Variável para armazenar o conteúdo do arquivo anterior
 
 //***************************** Funções *****************************//
-// Função de leitura do arquivo Automerge-Json
-function recompose(file, ext){
-  let output;
-  // Remove extensoes passadas no nome do arquivo nos argumentos
-  file = file.substr(0, file.lastIndexOf('.')) || file;
-  file = file+"."+ext
-  // Verifica se o arquivo existe localmente
-  if(!fs.existsSync(file)) {
-    if(verbose) console.log("Arquivo inexistente");
-  }
-  else {
-    // Carrega o arquivo
-    output = Automerge.load(fs.readFileSync(file, {encoding: null}));
-    return output;
 
-  }
+//Função para exibir o manual:
+function help() {
+
+console.log("AMPATCH v0.1.0")
+console.log("")
+console.log("AMPATCH is a command line utility to recompose an Automerge-JSON file from the differences between two files")
+console.log("")
+console.log("Usage:")
+console.log("    node ampatch.js <old_file> <diff_file> <new_file> [verbose]")
+console.log("    node ampatch.js help")
+console.log("")
+console.log("Help:")
+console.log("    Displays this manual")
+console.log("")
+console.log("Old_file:")
+console.log("    Name of previous file")
+console.log("")
+console.log("Diff_file:")
+console.log("    Name of the file containing the differences")
+console.log("")
+console.log("New_file:")
+console.log("    Name of the new file to be generated")
+console.log("")
+console.log("Verbose:")
+console.log("    Enable verbose mode")
+console.log("")
+console.log("More Information:")
+console.log("")
+console.log("    https://github.com/fabiobosisio/ampatch")
+console.log("")
+console.log("    Please report bugs at <https://github.com/fabiobosisio/ampatch/blob/master/README.md>")
+console.log("")
+}
+
+// Função de remoção das extensões dos arquivos
+function removext(file){
+  file = file.substr(0, file.lastIndexOf('.')) || file;
+  return file;
+}
+
+// Função de exibição do JSON formatado
+function show(out){
+  console.dir(out, { depth: null} );
+  return null;
+}
+
+//***************************** Módulo Principal *****************************//
+
+if(process.argv[5] == 'verbose'){ verbose = true;} // Habilita o modo verboso
+
+if (process.argv[2] == 'help' || process.argv[2] == 'Help' || process.argv[2] == 'h' || process.argv[2] == null  || process.argv[3] == null){ // Exibe o manual
+  help();
+  return null
+}
+
+fileant = removext(process.argv[2])+".am" // Adiciona a extensão ao arquivo origem
+if(!fs.existsSync(fileant)) { // Verifica se o arquivo existe localmente
+  console.log("Arquivo "+fileant+" inexistente");
+  return null
+}
+else{  
+  ant = Automerge.load(fs.readFileSync(fileant, {encoding: null}));// Carrega o arquivo automerge
+  if(verbose) {console.log('\x1b[36m%s\x1b[0m',"Arquivo anterior:"); show(ant);}
+}
+  
+filediff = removext(process.argv[3])+".diff"// Adiciona a extensão ao arquivo de diferença
+if(!fs.existsSync(filediff)) { // Verifica se o arquivo existe localmente
+  console.log('\x1b[1m\x1b[31m%s',"\nArquivo "+filediff+" inexistente!",'\x1b[0m\n');
+  
+  return null
+}
+else{
+  diff = JSON.parse(fs.readFileSync(filediff, {encoding: null})); // Carrega o arquivo de diferenças
+  if(verbose) console.log('\x1b[36m%s\x1b[0m',"Diferenca obtida com sucesso");
 }
  
-let ant = recompose(process.argv[2],'am')
-//let tp = recompose(process.argv[3], 'diff')
-let teste = fs.readFileSync(process.argv[3], {encoding: null})
-let diff = JSON.parse(teste)
-//let diff = Automerge.load(teste2);
- 			
-let temp = [];
-for (var y = 0; y < diff.length; y++) {
+let temp = []; // Variável de suporte
+for (var y = 0; y < diff.length; y++) { // Obtem o array binário
   temp.push(Uint8Array.from(Object.values(diff[y])));
 }
-let [Doc3, patch] = Automerge.applyChanges(ant, temp);
-content = Doc3;
-console.log(content);
 
-		
+let [content, patch] = Automerge.applyChanges(ant, temp);// Aplica as diferenças e constroi o arquivo 
 
-// Salva o arquivo local .automerge com os metadados automerge do json
-fs.writeFileSync(process.argv[4]+".am", Automerge.save(content), {encoding: null}); 
-fs.writeFileSync(process.argv[4]+".json", JSON.stringify(Automerge.load(process.argv[4]+".am")), {encoding: null}); 
-						
-//console.log('\x1b[36m%s\x1b[0m',`\nDiferenca entre o arquivo local e o conteudo da cadeia:`);
-//console.log(Automerge.load(filediff));
-
+if(!process.argv[4]) { // Verifica se o argumento foi passado
+  console.log('\x1b[1m\x1b[31m%s','\nNome de arquivo resultado nao informado, veja o manual!','\x1b[0m\n');
+  return null
+}
+else{
+  result = removext(process.argv[4])+".am"// Adiciona a extensão ao arquivo resultado  
+  fs.writeFileSync(result, Automerge.save(content), {encoding: null}); // Salva o arquivo local .automerge com os metadados automerge do json
+  if(verbose) {console.log('\x1b[36m%s\x1b[0m',"Arquivo resultado construido e salvo:"); show(content);}
+}
